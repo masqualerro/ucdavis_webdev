c96d83275f45a333f039edb0d3828cb2c20bf9c5
Miguel Sedillo <sedillomiguel.des@gmail.com>
2021-10-20 13:21:51 -0700

    added lj2 and increased lj font size

--- des157a/lj/script.js
+++ des157a/lj/script.js
@@ -9,23 +9,12 @@
 		liJournal[i].addEventListener("click", mySelect(i));
 	}
 	function mySelect(i) {
-		if (i === 0) {
-			return function () {
-				const cImg = document.createElement("img");
-				liJournal[i].appendChild(cImg);
-				cImg.setAttribute("src", "images/glyph.svg");
-				liJournal[i].style.fontWeight = "700";
-				liJournal[i].style.fontStyle = "italic";
-			};
-		}
-		if (i === 1) {
-			return function () {
-				const cImg = document.createElement("img");
-				liJournal[i].appendChild(cImg);
-				cImg.setAttribute("src", "images/glyph.svg");
-				liJournal[i].style.fontWeight = "700";
-				liJournal[i].style.fontStyle = "italic";
-			};
-		}
+		return function () {
+			const cImg = document.createElement("img");
+			liJournal[i].appendChild(cImg);
+			cImg.setAttribute("src", "images/glyph.svg");
+			liJournal[i].style.fontWeight = "700";
+			liJournal[i].style.fontStyle = "italic";
+		};
 	}
 })();
