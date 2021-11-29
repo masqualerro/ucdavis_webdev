bec10a9111847128278bef29d7faa99fc9524593
Miguel Sedillo <sedillomiguel.des@gmail.com>
2021-11-28 20:33:55 -0800

    animations + increased accessibility

--- des157a/final/proto/js/collect.js
+++ des157a/final/proto/js/collect.js
@@ -2,38 +2,46 @@
 	'use strict';
 	console.log('reading js: collection');
 
-	const foot = document.querySelector('footer');
+	const foot = document.getElementById('down');
+	const foot1 = document.getElementById('up');
 	const scrollContainer = document.getElementById('imgslide');
-	const downArrow = document.querySelector('.down');
 	const mosaic = document.getElementById('mosaic');
 	const footIcon = document.querySelector('.fas.fa-chevron-down');
-	const upArrow = document.querySelector('.up');
+	const footIcon1 = document.querySelector('.fas.fa-chevron-up');
 
 	foot.addEventListener('click', function () {
 		mosaic.style.transform = 'translateY(-100%)';
 		console.log('clicked');
 		scrollContainer.style.opacity = '0%';
-		footIcon.className = 'fas fa-chevron-up';
-		foot.className = 'up';
+		foot.style.display = 'none';
+		foot1.style.display = 'block';
 	});
 
-	// foot.addEventListener('click', function () {
-	mosaic.style.transform = 'translateY(100%)';
-	console.log('clicked 78');
-	scrollContainer.style.opacity = '100%';
-	footIcon.className = 'fas fa-chevron-down';
-	foot.className = 'down';
+	foot1.addEventListener('click', function () {
+		mosaic.style.transform = 'translateY(100%)';
+		console.log('clicked 2');
+		scrollContainer.style.opacity = '100%';
+		foot.style.display = 'block';
+		foot1.style.display = 'none';
+	});
+
+	// foot.addEventListener('mouseover', function () {
+	// 	footIcon.style.transform = 'translateY(-5px)';
+	// 	footIcon.style.color = '#f241c3';
+	// });
+	// foot1.addEventListener('mouseover', function () {
+	// 	footIcon1.style.transform = 'translateY(-5px)';
+	// 	footIcon1.style.color = '#f241c3';
 	// });
 
-	foot.addEventListener('mouseover', function () {
-		footIcon.style.transform = 'translateY(-5px)';
-		footIcon.style.color = '#f241c3';
-	});
-
-	foot.addEventListener('mouseout', function () {
-		footIcon.style.transform = 'translateY(0px)';
-		footIcon.style.color = 'black';
-	});
+	// foot.addEventListener('mouseout', function () {
+	// 	footIcon.style.transform = 'translateY(0px)';
+	// 	footIcon.style.color = 'black';
+	// });
+	// foot1.addEventListener('mouseout', function () {
+	// 	footIcon1.style.transform = 'translateY(0px)';
+	// 	footIcon1.style.color = 'black';
+	// });
 
 	scrollContainer.addEventListener('wheel', (evt) => {
 		evt.preventDefault();
